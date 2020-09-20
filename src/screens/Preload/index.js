4cc7e2ae4c32a39f660e3bdb62ac1fb1a5f7a08b
freddneos <fredd@neosdev.com.br>
2020-09-20 17:26:00 +0100

    feat : check token and navigation

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,11 +1,28 @@
-import React from "react";
-import { Text } from "react-native";
-import { Container } from "./styles";
+import AsyncStorage from "@react-native-community/async-storage";
+import React, { useEffect } from "react";
+import { Container, LoadingIcon } from "./styles";
+
+import Logo from "../../assets/doctor_hearth_beat.svg";
+import { useNavigation } from "@react-navigation/native";
 
 export default () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("token");
+      if (token) {
+        //validate token
+      } else {
+        navigation.navigate("SignIn");
+      }
+    };
+    checkToken();
+  }, []);
+
   return (
     <Container>
-      <Text>Preload</Text>
+      <Logo width="60%" height="60%" />
+      <LoadingIcon size="large" color="#FFF" />
     </Container>
   );
-}
+};
